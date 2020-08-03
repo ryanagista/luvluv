6f02491e78e5e9291f1b05c1ffe97d9cfa026e6d
ryanagista <agistafakturryan>
2020-08-03 08:10:17 +0700

    luvluv

---
+++ assets/quotes.js
@@ -0,0 +1,11 @@
+// prettier-ignore
+export default [
+    { author: "ryan", quotes: "Whatever happens, keep breathing" },
+    { author: "ryan", quotes: "I will always wait for you\nWhenever and wherever you are" },
+    { author: "ryan", quotes: "Lia jangan sedih,biar mas saja." },
+    { author: "ryan", quotes: "I love you." },
+    { author: "ryan", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "ryan", quotes: "I love you." },
+    { author: "ryan", quotes: "I'm sorry for loving you." },
+    { author: "ryan", quotes: "Semangat selalu sayangku!" },
+];
---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["panda", "Sayangku", "Sayang", "kang nyeblak", "Liaku :3"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Jangan lupain mas ya,sholatnya dijaga ya",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
